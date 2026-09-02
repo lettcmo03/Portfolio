8601c8bf9a2f5b3062a385ee7e13b4614881596d
lettcmo03 <166147615+lettcmo03@users.noreply.github.com>
2026-09-01 21:09:11 -0300

    added sobre & habilidades

--- src/App.tsx
+++ src/App.tsx
@@ -69,6 +69,34 @@ return(
           </h2>
         </div>
       </section>
+      <section className='sobre' id='sobre'>
+        <div className="sobre-conteudo">
+          <h2>Sobre Mim</h2>
+          <p>Sou estudante de Engenharia de Software e venho construindo minha experiência por meio de projetos academicos e pessoais</p>
+          <p>Tenho interesse principalmente em desenvolvimento de sistemas, com foco em back-end, e atualmente estudo e desenvolvo projetos utilizando tecnologias como: Java, Python, C, e atualmete estou me aprofundando no Front com HTML, CSS e React.</p>
+          <p>Gosto de transormar o que aprendo em projetos práticos buscando evoluir tanto na programação quanto na organização econstrução de soluções completas</p>
+        </div>
+      </section>
+      <section className='experiencias' id='projetos'>
+        <h2>Minhas Experiências</h2>
+        <div className="cards-experiencias">
+          <div className="card">
+            <div className="card-imagem">/AQUI VAI ENTRAR A IMAGEM DO PROJETO/</div>
+            <h3>Conecta 60+</h3>
+            <p>Projeto voltado para a inclusão e alfabetização digital de pessoas com 60 ou mais anos, desenvolvido em ambiente acadêmico.</p>
+          </div>
+          <div className="card">
+            <div className="card-imagem">/AQUI VAI ENTRAR A IMAGEM DO PROJETO/</div>
+            <h3>Projetos de Acadêmicos</h3>
+            <p>Desenvolvimento de aplicações acadêmicas, utilizando linguagens como Python e C</p>
+          </div>
+          <div className="card">
+            <div className="card-imagem">/AQUI VAI ENTRAR A IMAGEM DO PROJETO/</div>
+            <h3>Projetos Pessoais</h3>
+            <p>Desenvolvimento de aplicações gerais para melhorar lógica de programação, versionamento de projetos e conhecimentos gerais, utilizando linguagens como Python e C</p>
+          </div>
+        </div>
+      </section>
     </main>
   </>
 )
